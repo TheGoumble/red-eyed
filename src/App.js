ea3876e35e9a40a1446764c836674b0e93428df4
Javier Vargas <3231javiervargas@gmail.com>
2023-02-27 23:23:12 -0500

    setting up basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Email from "./pages/Email"
+import Home from "./pages/Home"
+import Music from "./pages/Music"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="music" element={<Music />} />
+        <Route path="email" element={<Email />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Email/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Email = () => {
+  return (
+  <>
+  
+  </>
+  )
+}
+
+export default Email
